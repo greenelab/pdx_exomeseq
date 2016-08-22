028eae76778390adb148de4df093abbd7d58dba0
gwaygenomics <gregway@mail.med.upenn.edu>
2016-08-22 16:22:03 -0400

    add depends to install script

--- install.sh
+++ install.sh
@@ -11,5 +11,34 @@
 # Install Burrows Wheeler Aligner and make hg19 index files
 #wget --directory-prefix modules/ https://sourceforge.net/projects/bio-bwa/files/bwa-0.7.5a.tar.bz2 
 #tar -vxjf modules/bwa-0.7.5a.tar.bz2 -C modules/ && make --directory modules/bwa-0.7.5a
-modules/bwa-0.7.5a/bwa index -a bwtsw "reference/ucsc.hg19.fasta"
+#modules/bwa-0.7.5a/bwa index -a bwtsw "reference/ucsc.hg19.fasta"
 
+# DEPENDENCIES
+
+# FastQC
+#wget --directory-prefix modules/ http://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v0.10.1.zip
+#unzip modules/fastqc_v0.10.1.zip -d modules/
+#chmod +x modules/FastQC/fastqc
+
+# picard
+#wget --directory-prefix modules/ https://sourceforge.net/projects/picard/files/picard-tools-1.105.zip
+#unzip modules/picard-tools-1.105.zip -d modules/
+
+# SAM tools
+#wget --directory-prefix modules/ https://sourceforge.net/projects/samtools/files/samtools-0.1.19.tar.bz2
+#tar -vxjf modules/samtools-0.1.19.tar.bz2 -C modules/ && make --directory modules/samtools-0.1.19
+
+# GATK
+#wget --directory-prefix modules/ https://software.broadinstitute.org/gatk/download/auth?package=GATK-archive&version=2.8-1-g932cd3a
+
+# vcftools
+#wget --directory-prefix modules/ http://pkgs.fedoraproject.org/repo/pkgs/vcftools/vcftools_0.1.11.tar.gz/ddb49e9fa2bfedae36b4dce163adfaa8/vcftools_0.1.11.tar.gz
+#tar -xvf modules/vcftools_0.1.11.tar.gz -C modules/ && make --directory modules/vcftools_0.1.11
+
+# tabix
+#wget --directory-prefix modules/ https://sourceforge.net/projects/samtools/files/tabix/tabix-0.2.6.tar.bz2
+#tar -vxjf modules/tabix-0.2.6.tar.bz2 -C modules/ && make --directory modules/tabix-0.2.6
+
+# SNVer
+wget --directory-prefix modules/ https://sourceforge.net/projects/snver/files/SNVer-0.5.3.tar.gz
+tar -xvf modules/SNVer-0.5.3.tar.gz -C modules/
