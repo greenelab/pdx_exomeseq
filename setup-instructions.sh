2a7488fab9961dec663df88353e6bbba263b0e6a
gwaygenomics <gregway@mail.med.upenn.edu>
2017-04-13 13:19:12 -0400

    add instructions for setting up environment

---
+++ setup-instructions.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# setting up the conda environment is tricky on the cluster
+
+# Step 1: load the python anaconda module
+m load python/3.5-Anaconda
+
+# Step 2: activate the conda environment
+source activate pdx-exomeseq
+
+# Step 3: Unload the anaconda module
+m unload python/3.5-Anaconda
+
+# Step 4: Setup channels
+conda config --add channels conda-forge
+conda config --add channels defaults
+conda config --add channels r
+conda config --add channels bioconda
+
+# Step 5: Install bioconda packages
+conda install ngs-disambiguate
+
