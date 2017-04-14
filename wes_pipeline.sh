6dccc901369616ca197f3bf9a8f1ffeb85bae917
gwaygenomics <gregway@mail.med.upenn.edu>
2017-04-13 22:06:38 -0400

    update wes pipeline

--- wes_pipeline.sh
+++ wes_pipeline.sh
@@ -2,11 +2,12 @@
 
 # All the steps in order that have been run
 
-# Fastqc on all samples
-python util/submit_qc_reports.py
+# FastQC on all samples
+python util/submit_qc_reports.py --data_dir 'data'
 
 # Run TrimGalore to cut adapters and filter low quality reads
-# First make sure fastqc is in the PATH
-export PATH=/lorax/sanchezlab/shared/pdx_exomeseq/modules/FastQC:$PATH
 python util/run_trimgalore.py
+
+# Run FastQC on the trimmed data
+python util/submit_qc_reports.py --data_dir 'data/trimmed'
 
