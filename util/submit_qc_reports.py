acd7b9a22ef0c31009f9b6253f5d6ceb149e0250
gwaygenomics <gregway@mail.med.upenn.edu>
2017-04-13 22:06:14 -0400

    add path option for qc report helper

--- util/submit_qc_reports.py
+++ util/submit_qc_reports.py
@@ -6,12 +6,20 @@ Call file to perform FastQC on all files each as separate jobs
 """
 
 import os
+import argparse
 import subprocess
 
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', '--data_dir',
+                    help='Location to search for files eligible for QC')
+args = parser.parse_args()
+data_dir = args.data_dir
+
 wes_files = []
-for path, subdirs, files in os.walk('data'):
+for path, subdirs, files in os.walk(data_dir):
     for name in files:
-        wes_files.append(os.path.join(path, name))
+        if 'fasta.gz' in name or 'fq.gz' in name:
+            wes_files.append(os.path.join(path, name))
 
 for data_file in wes_files:
     base_name = os.path.basename(data_file)
