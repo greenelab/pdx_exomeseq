06916a8afadcd11d8bfe3da0c56e119cdf5345fe
gwaygenomics <gregway@mail.med.upenn.edu>
2017-11-03 15:27:28 -0400

    update to os path join in bwa mem script;

--- scripts/bwa_mem.py
+++ scripts/bwa_mem.py
@@ -9,7 +9,8 @@ import os
 import subprocess
 
 all_files = []
-for path, subdirs, files in os.walk('data/trimmed'):
+data_dir = os.path.join('data', 'trimmed')
+for path, subdirs, files in os.walk(data_dir):
     for name in files:
         if 'trimmed.fq.gz' in name:
             all_files.append(name)
@@ -19,11 +20,12 @@ for name in all_files:
     if '_R1_' in name:
         read_1 = name
         read_2 = name.replace('_R1_', '_R2_')
-        paired_reads.append(['data/trimmed/' + read_1,
-                             'data/trimmed/' + read_2])
+        paired_reads.append([os.path.join(data_dir, read_1),
+                             os.patj.join(data_dir, read_2)])
 
 command_util = os.path.join('util', 'command_wrapper.py')
 for sample_1, sample_2 in paired_reads:
     command = ['python', command_util, '--sample_1', sample_1,
                '--sample_2', sample_2, '--command', 'mem']
     subprocess.call(command)
+
