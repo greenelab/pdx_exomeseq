7f8acc17e80f12a9000e5e7a5b86087634e0650a
gwaygenomics <gregway@mail.med.upenn.edu>
2017-11-03 16:03:54 -0400

    update script calls to pipeline

--- wes_pipeline.sh
+++ wes_pipeline.sh
@@ -3,11 +3,11 @@
 # All the steps in order that have been run
 
 # FastQC on all samples
-python util/submit_qc_reports.py --data_dir 'data'
+python scripts/1.run_fastqc.py --data_dir 'data' --output_dir 'data/fastqc_raw'
 
 # Run TrimGalore to cut adapters and filter low quality reads
-python util/run_trimgalore.py
+python scripts/2.run_trimgalore.py --data_dir 'data' --output_dir 'data/trimmed'
 
 # Run FastQC on the trimmed data
-python util/submit_qc_reports.py --data_dir 'data/trimmed'
+python scripts/1.run_fastqc.py --data_dir 'data/trimmed' --output_dir 'data/fastqc_trimmed'
 
