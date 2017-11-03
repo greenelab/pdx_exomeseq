9ce33766a8f16ef3431a51a32a79797083f41569
gwaygenomics <gregway@mail.med.upenn.edu>
2017-11-03 15:30:54 -0400

    update submit qc script

--- util/submit_qc_reports.py
+++ util/submit_qc_reports.py
@@ -21,12 +21,10 @@ for path, subdirs, files in os.walk(data_dir):
         if 'fasta.gz' in name or 'fq.gz' in name:
             wes_files.append(os.path.join(path, name))
 
+command_util = os.path.join('util', 'command_wrapper.py')
 for data_file in wes_files:
     base_name = os.path.basename(data_file)
-    command = ['python', os.path.join('util', 'schedule.py'),
-               '--command', 'modules/FastQC/fastqc ' + data_file,
-               '--name', 'fastqc_' + base_name,
-               '--walltime', '01:30:00',
-               '--filename', 'fastqc_' + base_name + '.pbs']
+    command = ['python', command_util, '--sample_1', base_name,
+               '--command' 'fastqc']
     subprocess.call(command)
 
