9de3eaca13eb3e5e832ecea8675f53a954dd93f6
gwaygenomics <gregory.way@gmail.com>
2017-11-07 15:41:11 -0500

    add to skeleton pipeline with intended commands

--- wes_pipeline.sh
+++ wes_pipeline.sh
@@ -11,3 +11,20 @@ python scripts/2.run_trimgalore.py --data_dir 'data' --output_dir 'data/trimmed'
 # Run FastQC on the trimmed data
 python scripts/1.run_fastqc.py --data_dir 'data/trimmed' --output_dir 'data/fastqc_trimmed'
 
+# Align reads to reference (hg19)
+python scripts/3.run_bwa.py --data_dir 'data/trimmed' --command 'mem'
+
+# Combine paired end reads
+python scripts/3.run_bwa.py --data_dir 'data/trimmed' --command 'sampe'
+
+# Prep to remove duplicate reads
+python scripts/4.run_samtools.py --data_dir 'data/trimmed' --command 'fixmate'
+
+# Prep to remove duplicate reads
+python scripts/4.run_samtools.py --data_dir 'data/trimmed' --command 'sort_order'
+
+# Remove duplicate reads
+python scripts/4.run_samtools.py --data_dir 'data/trimmed' --command 'markdup'
+
+# Adding read groups with picard
+python scripts/5.run_picard.py --data_dir 'data/trimmed' --command 'addreadgroups'
