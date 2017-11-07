1db908d984f09cd3b493909c82fd1bb79e97afc6
gwaygenomics <gregory.way@gmail.com>
2017-11-07 11:52:59 -0500

    fix typo

--- scripts/2.run_trimgalore.py
+++ scripts/2.run_trimgalore.py
@@ -12,14 +12,14 @@ import subprocess
 parser = argparse.ArgumentParser()
 parser.add_argument('-f', '--data_dir',
                     help='Location of input data')
-parser.add_arguemtn('-o', '--output_dir',
+parser.add_argument('-o', '--output_dir',
                     help='Location to save output data')
 parser.add_argument('-y', '--config_yaml',
                     help='Configuration variables for input',
                     default='discovery_variables.yml')
 parser.add_argument('-w', '--walltime',
                     help='the amount of time alloted to the script')
-parser.add_argument('-o', '--nodes', default=1,
+parser.add_argument('-n', '--nodes', default=1,
                     help='the number of nodes to allocate')
 parser.add_argument('-r', '--cores', default=1,
                     help='the number of cores to allocate per node')
