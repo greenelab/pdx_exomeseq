2394d1f968a5f9bf675e35f6db0309d2f527261d
gwaygenomics <gregway@mail.med.upenn.edu>
2017-11-08 10:39:44 -0500

    update bwa to conda

--- install.sh
+++ install.sh
@@ -1,9 +1,3 @@
-# Here are instructions to install fastq2vcf and dependencies
-wget https://sourceforge.net/projects/fastq2vcf/files/latest/download/fastq2vcf_v15.zip
-unzip fastq2vcf_v15.zip
-rm fastq2vcf_v15.zip
-rm -r __MAC*
-
 # Download hg19 genome and generate bwa index files
 wget --directory-prefix reference/ ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/2.8/hg19/ucsc.hg19.fasta.gz
 gzip --decompress reference/ucsc.hg19.fasta.gz
@@ -13,7 +7,7 @@ wget --directory-prefix reference http://hgdownload.cse.ucsc.edu/goldenPath/hg19
 
 # Download mm9 genome
 wget --directory-prefix reference http://hgdownload.cse.ucsc.edu/goldenPath/mm9/bigZips/mm9.2bit
-#wget --directory-prefix tools http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/twoBitToFa
+wget --directory-prefix tools http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/twoBitToFa
 
 # Convert 2bit file to fa
 cd tools
@@ -32,11 +26,8 @@ module load blast+/2.6.0
 makeblastdb -in reference/combined_g1k_v37_mm9.fasta -parse_seqids -dbtype nucl
 
 # Install Burrows Wheeler Aligner and make hg19 index files
-wget --directory-prefix modules/ https://sourceforge.net/projects/bio-bwa/files/bwa-0.7.5a.tar.bz2 
-tar -vxjf modules/bwa-0.7.5a.tar.bz2 -C modules/ && make --directory modules/bwa-0.7.5a
-modules/bwa-0.7.5a/bwa index -a bwtsw "reference/ucsc.hg19.fasta"
-python util/schedule.py --command 'modules/bwa-0.7.5a/bwa index -a bwtsw "reference/hg19.fa"' --name 'hg19-bwa' --walltime '02:00:00' --filename 'bwa-hg19-index'
-python util/schedule.py --command 'modules/bwa-0.7.5a/bwa index -a bwtsw "reference/mm9.fa"' --name 'mm9-bwa' --walltime '02:00:00' --filename 'bwa-mm9-index'
+python util/schedule.py --command '/ihome/gway/.conda/envs/pdx-exomeseq/bin/bwa index -a bwtsw "reference/human_g1k_v37.fasta"' --name 'hg19-bwa' --walltime '02:00:00' --filename 'bwa-hg19-index.pbs'
+python util/schedule.py --command '/ihome/gway/.conda/envs/pdx-exomeseq/bin/bwa index -a bwtsw "reference/mm9.fa"' --name 'mm9-bwa' --walltime '02:00:00' --filename 'bwa-mm9-index.pbs'
 
 # DEPENDENCIES
 
@@ -55,16 +46,16 @@ python util/schedule.py --command 'modules/bwa-0.7.5a/bwa index -a bwtsw "refere
 # included in the conda environment
 
 # vcftools
-wget --directory-prefix modules/ http://pkgs.fedoraproject.org/repo/pkgs/vcftools/vcftools_0.1.11.tar.gz/ddb49e9fa2bfedae36b4dce163adfaa8/vcftools_0.1.11.tar.gz
-tar -xvf modules/vcftools_0.1.11.tar.gz -C modules/ && make --directory modules/vcftools_0.1.11
+#wget --directory-prefix modules/ http://pkgs.fedoraproject.org/repo/pkgs/vcftools/vcftools_0.1.11.tar.gz/ddb49e9fa2bfedae36b4dce163adfaa8/vcftools_0.1.11.tar.gz
+#tar -xvf modules/vcftools_0.1.11.tar.gz -C modules/ && make --directory modules/vcftools_0.1.11
 
 # tabix
-wget --directory-prefix modules/ https://sourceforge.net/projects/samtools/files/tabix/tabix-0.2.6.tar.bz2
-tar -vxjf modules/tabix-0.2.6.tar.bz2 -C modules/ && make --directory modules/tabix-0.2.6
+#wget --directory-prefix modules/ https://sourceforge.net/projects/samtools/files/tabix/tabix-0.2.6.tar.bz2
+#tar -vxjf modules/tabix-0.2.6.tar.bz2 -C modules/ && make --directory modules/tabix-0.2.6
 
 # SNVer
-wget --directory-prefix modules/ https://sourceforge.net/projects/snver/files/SNVer-0.5.3.tar.gz
-tar -xvf modules/SNVer-0.5.3.tar.gz -C modules/
+#wget --directory-prefix modules/ https://sourceforge.net/projects/snver/files/SNVer-0.5.3.tar.gz
+#tar -xvf modules/SNVer-0.5.3.tar.gz -C modules/
 
 # MAPEXR
 wget --directory-prefix modules/ https://bitbucket.org/bmannakee/mapexr/get/da36687d4585.zip
