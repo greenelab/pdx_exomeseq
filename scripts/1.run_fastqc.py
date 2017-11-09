da2d5be58d56047690b748517460dbf1ce04fcd7
gwaygenomics <gregory.way@gmail.com>
2017-11-09 10:32:06 -0500

    testing holding out files from fastqc step

--- scripts/1.run_fastqc.py
+++ scripts/1.run_fastqc.py
@@ -8,6 +8,7 @@ Call file to perform FastQC on all files each as separate jobs
 import os
 import argparse
 import subprocess
+import pandas as pd
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-f', '--data_dir',
@@ -23,6 +24,8 @@ parser.add_argument('-n', '--nodes', default=1,
                     help='the number of nodes to allocate')
 parser.add_argument('-r', '--cores', default=4,
                     help='the number of cores to allocate per node')
+parser.add_argument('-p', '--phenotype', default='pdx_phenotype.csv',
+                    help='the filename where the phenotype data is saved')
 args = parser.parse_args()
 
 data_dir = args.data_dir
@@ -31,13 +34,20 @@ config = args.config_yaml
 walltime = args.walltime
 nodes = args.nodes
 cores = args.cores
+phenotype_file = args.phenotype
+
+phenotype_df = pd.read_csv(phenotype_file, index_col=0)
 
 wes_files = []
+existing_files = os.listdir(out_dir)
 for path, subdirs, files in os.walk(data_dir):
     for name in files:
-        if 'fasta.gz' in name or 'fq.gz' in name:
-            wes_files.append(os.path.join(path, name))
-
+        if 'fasta.gz' in name or 'fq.gz' in name or 'fastq.gz' in name:
+            full_name = os.path.join(path, name)
+            fastqc_name = os.path.join(out_dir, '{}_fastqc.html'.format(name))
+            if fastqc_name not in existing_files:
+                wes_files.append(fastqc_name)
+print(wes_files)
 command_util = os.path.join('util', 'command_wrapper.py')
 for data_file in wes_files:
     command = ['python', command_util,
@@ -48,4 +58,4 @@ for data_file in wes_files:
                '--walltime', walltime,
                '--nodes', nodes,
                '--cores', cores]
-    subprocess.call(command)
+    #subprocess.call(command)
