f8a245dc1a7c6773fe93c95d33e86e7470923ead
gwaygenomics <gregory.way@gmail.com>
2017-11-10 09:17:33 -0500

    add samtools running in pipeline script

--- wes_pipeline.sh
+++ wes_pipeline.sh
@@ -2,6 +2,9 @@
 
 # All the steps in order that have been run
 
+###################
+# STEP 1
+###################
 # FastQC on all samples
 # python scripts/1.run_fastqc.py --data_dir 'data' --output_dir 'results/fastqc_raw' \
 #         --walltime '01:30:00' --nodes 1 --cores 4
@@ -9,6 +12,9 @@
 # Run MultiQC on FastQC results
 # multiqc results/fastqc_raw/ --force --filename 'results/multiqc_report_raw.html'
 
+###################
+# STEP 2
+###################
 # Run TrimGalore to cut adapters and filter low quality reads
 # TrimGalore is run in `--paired` mode, which performs an additional filtering step
 # on low-quality read pairs between samples. Both pairs of the reads must have greater
@@ -19,12 +25,19 @@
 # Run MultiQC again on Trimmed FastQC results from the trimgalore step
 # multiqc results/fastqc_trimmed/ --force --filename 'results/multiqc_report_trimmed.html'
 
+###################
+# STEP 3
+###################
 # Align reads to reference (hg19)
-python scripts/3.run_bwa.py --data_dir 'processed/trimmed' --output_dir 'processed/sam' \
-        --command 'mem' --walltime '04:00:00' --nodes 1 --cores 8
+# python scripts/3.run_bwa.py --data_dir 'processed/trimmed' --output_dir 'processed/sam' \
+#        --command 'mem' --walltime '04:00:00' --nodes 1 --cores 8
 
-# Combine paired end reads
-# python scripts/3.run_bwa.py --data_dir 'data/trimmed' --command 'sampe'
+###################
+# STEP 4
+###################
+# Sort SAM and convert to BAM
+python scripts/4.run_samtools.py --data_dir 'processed/sam' --output_dir 'processed/bam' \
+        --walltime '02:00:00' --nodes 2 --cores 12
 
 # Prep to remove duplicate reads
 # python scripts/4.run_samtools.py --data_dir 'data/trimmed' --command 'fixmate'
