085cc6f6ec719164d2716b3e4e558c8cdae083d5
gwaygenomics <gregory.way@gmail.com>
2017-11-13 10:37:25 -0500

    add variant calling script

---
+++ scripts/5.variant_calling.py
@@ -0,0 +1,51 @@
+"""
+Gregory Way 2017
+scripts/4.rum_samtools.py
+
+Will submit jobs to run `samtools` on all samples
+"""
+
+import os
+import argparse
+import subprocess
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', '--data_dir',
+                    help='Location of input data')
+parser.add_argument('-o', '--output_dir',
+                    help='Location to save output data',
+                    default='.')
+parser.add_argument('-c', '--command',
+                    help='One of the samtools commands')
+parser.add_argument('-y', '--config_yaml',
+                    help='Configuration variables for input',
+                    default='discovery_variables.yml')
+parser.add_argument('-w', '--walltime',
+                    help='the amount of time alloted to the script')
+parser.add_argument('-n', '--nodes', default=1,
+                    help='the number of nodes to allocate')
+parser.add_argument('-r', '--cores', default=1,
+                    help='the number of cores to allocate per node')
+args = parser.parse_args()
+
+data_dir = args.data_dir
+out_dir = args.output_dir
+command = args.command
+config = args.config_yaml
+walltime = args.walltime
+nodes = str(args.nodes)
+cores = str(args.cores)
+
+sam_files = []
+for path, subdirs, files in os.walk(data_dir):
+    for name in files:
+        if 'fq.gz.sam' in name or '.sam_sorted.bam' in name:
+            sam_files.append(name)
+
+command_util = os.path.join('util', 'command_wrapper.py')
+for sample_1 in sam_files:
+    com = ['python', command_util, '--sample', sample_1,
+           '--command', command, '--output_directory', out_dir,
+           '--config_yaml', config, '--walltime', walltime,
+           '--nodes', nodes, '--cores', cores]
+    subprocess.call(com)
