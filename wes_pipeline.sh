b7424887468c69f432879e7875c925790701d81c
gwaygenomics <gregory.way@gmail.com>
2017-11-13 10:37:11 -0500

    update step numbers and add variant calling

--- wes_pipeline.sh
+++ wes_pipeline.sh
@@ -3,7 +3,7 @@
 # All the steps in order that have been run
 
 ###################
-# STEP 1
+# STEP 1 - Quality Control
 ###################
 # FastQC on all samples
 # python scripts/1.run_fastqc.py --data_dir 'data' --output_dir 'results/fastqc_raw' \
@@ -12,9 +12,6 @@
 # Run MultiQC on FastQC results
 # multiqc results/fastqc_raw/ --force --filename 'results/multiqc_report_raw.html'
 
-###################
-# STEP 2
-###################
 # Run TrimGalore to cut adapters and filter low quality reads
 # TrimGalore is run in `--paired` mode, which performs an additional filtering step
 # on low-quality read pairs between samples. Both pairs of the reads must have greater
@@ -26,14 +23,14 @@
 # multiqc results/fastqc_trimmed/ --force --filename 'results/multiqc_report_trimmed.html'
 
 ###################
-# STEP 3
+# STEP 2 - Alignment
 ###################
 # Align reads to reference (hg19)
 # python scripts/3.run_bwa.py --data_dir 'processed/trimmed' --output_dir 'processed/sam' \
 #        --command 'mem' --walltime '04:00:00' --nodes 1 --cores 8
 
 ###################
-# STEP 4
+# STEP 3 - Data Conversion and Processing
 ###################
 # Sort SAM and convert to BAM
 # python scripts/4.run_samtools.py --command 'sort_name' --data_dir 'processed/sam' \
@@ -52,10 +49,11 @@
 #       --output_dir 'processed/bam_rmdup' --walltime '02:30:00' --nodes 1 --cores 8
 
 # Create BAM index for duplicate removal
-python scripts/4.run_samtools.py --command 'index_bam' --data_dir 'processed/bam_rmdup' \
-        --output_dir 'processed/bam_rmdup' --walltime '02:30:00' --nodes 1 --cores 8
+# python scripts/4.run_samtools.py --command 'index_bam' --data_dir 'processed/bam_rmdup' \
+#        --output_dir 'processed/bam_rmdup' --walltime '02:30:00' --nodes 1 --cores 8
 
-
-
-# Adding read groups with picard
-# python scripts/5.run_picard.py --data_dir 'data/trimmed' --command 'addreadgroups'
+###################
+# STEP 4 - Variant Calling
+###################
+python scripts/5.variant_calling.py --command 'target_realign' --data_dir 'processed/bam_rmdup' \
+        --output_dir 'processed/bam_indel_realign' --walltime '03:00:00' --nodes 1 --cores 8
