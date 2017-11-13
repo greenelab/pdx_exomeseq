55246658daaba8097419aa538c8995d24a5044b8
gwaygenomics <gregway@mail.med.upenn.edu>
2017-11-13 12:43:20 -0500

    variant calling test

--- scripts/5.variant_calling.py
+++ scripts/5.variant_calling.py
@@ -41,7 +41,7 @@ for path, subdirs, files in os.walk(data_dir):
     for name in files:
         if 'bam.bam_rmdup.bam' in name and 'bam_rmdup.bam.bai' not in name:
             bam_files.append(name)
-
+bam_files = bam_files[0:2]
 command_util = os.path.join('util', 'command_wrapper.py')
 for sample_1 in bam_files:
     com = ['python', command_util, '--sample', sample_1,
