b018aef3c21f6dcb94b4773d8cbae654eeef5395
gwaygenomics <gregory.way@gmail.com>
2017-11-14 12:37:57 -0500

    update mapexr script args

--- scripts/6.run_mapexr.py
+++ scripts/6.run_mapexr.py
@@ -12,11 +12,17 @@ import subprocess
 parser = argparse.ArgumentParser()
 parser.add_argument('-f', '--data_dir',
                     help='Location of input data')
+parser.add_argument('-i', '--mapex_path_to_bam_index',
+                    help='location of the BAM file index to use for mapex')
+parser.add_argument('-v', '--mapex_path_to_vcf',
+                    help='location of the VCF file to use for mapex')
 parser.add_argument('-o', '--output_dir',
                     help='Location to save output data',
                     default='.')
-parser.add_argument('-c', '--command',
-                    help='One of the samtools commands')
+parser.add_argument('-l', '--mapex_path_to_blast_output',
+                    help='location of the BLAST output file of mapex run')
+parser.add_argument('-v', '--vcf_dir',
+                    help='location of the VCF results directory')
 parser.add_argument('-y', '--config_yaml',
                     help='Configuration variables for input',
                     default='discovery_variables.yml')
@@ -30,7 +36,8 @@ args = parser.parse_args()
 
 data_dir = args.data_dir
 out_dir = args.output_dir
-command = args.command
+vcf_dir = args.vcf_dir
+blast_out_dir = args.mapex_path_to_blast_output
 config = args.config_yaml
 walltime = args.walltime
 nodes = str(args.nodes)
@@ -42,10 +49,20 @@ for path, subdirs, files in os.walk(data_dir):
         if 'bam.bam_rmdup.bam' in name and '.bam.bai' not in name:
             bam_files.append(name)
 
+# sample_files = [sample_base, sample_bam, sample_bai, sample_vcf]
+
+
 command_util = os.path.join('util', 'command_wrapper.py')
 for sample_1 in bam_files:
-    com = ['python', command_util, '--sample', sample_1,
-           '--command', command, '--output_directory', out_dir,
-           '--config_yaml', config, '--walltime', walltime,
-           '--nodes', nodes, '--cores', cores]
+    com = ['python', command_util,
+           '--sample', sample_1,
+           '--command', 'mapex',
+           '--mapex_path_to_bam_index', data_dir,
+           '--mapex_path_to_vcf', vcf_dir,
+           '--mapex_path_to_blast_output', blast_out_dir,
+           '--output_directory', out_dir,
+           '--config_yaml', config,
+           '--walltime', walltime,
+           '--nodes', nodes,
+           '--cores', cores]
     subprocess.call(com)
