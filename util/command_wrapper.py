b9f5471209b22bc7c811553f32edaf0ac67b9f84
Greg <gregory.way@gmail.com>
2017-11-15 13:07:09 -0500

    refactor

--- util/command_wrapper.py
+++ util/command_wrapper.py
@@ -18,7 +18,10 @@ parser.add_argument('-b', '--sample_2', default=None,
                     help='Second sample of interest')
 parser.add_argument('-c', '--command',
                     help='which command to run. Can be one of: \
-                         ["fastqc", "trimgalore", "mem", "sort", "markdup"]')
+                         ["fastqc", "trimgalore", "mem", "sort_name", \
+                          "sort_position", "fixmate", "rmdup", "index_bam", \
+                          "index_bam_gatk", "add_read_groups", \
+                          "mutect2", "mapex"]')
 parser.add_argument('-o', '--output_directory',
                     help='the location to save the output files')
 parser.add_argument('-y', '--config_yaml', default='discovery_variables.yml',
@@ -90,19 +93,17 @@ schedule_name = '{}_{}'.format(os.path.basename(sample_1), command)
 sample_name = sample_1.replace('_R1_', '_').replace('_val_1', '')
 sample_base = os.path.join(base_dir, output_dir, sample_name)
 
-# Output files
-sample_sam = sample_base + '.sam'
-sample_sorted_bam = sample_base + '_sorted'
-sample_sorted_fixmate_bam = sample_base + '_sorted_fixmate.bam'
-sample_sorted_positionsort_bam = sample_base + '_positionsort.bam'
-sample_markdup_bam = sample_base + '_rmdup.bam'
-sample_markdup_bai = sample_base + '.bai'
-sample_indel_intervals = sample_base + '.intervals'
-sample_indel_realign = sample_base + 'realigned.bam'
-sample_addreadgroup = sample_base + '.rg.bam'
-sample_bamindex_gatk = sample_base + '.bai'
-sample_gatk_vcf = sample_base + '.GATK.vcf'
-sample_mapex_out = sample_base + '.tsv'
+# Output file suffixes
+sample_sam = '{}.sam'.format(sample_base)
+sample_sorted_bam = '{}_sorted'.format(sample_base)
+sample_sorted_fixmate_bam = '{}_sorted_fixmate.bam'.format(sample_base)
+sample_sorted_positionsort_bam = '{}_positionsort.bam'.format(sample_base)
+sample_markdup_bam = '{}_rmdup.bam'.format(sample_base)
+sample_markdup_bai = '{}.bai'.format(sample_base)
+sample_addreadgroup = '{}.rg.bam'.format(sample_base)
+sample_bamindex_gatk = '{}.bai'.format(sample_base)
+sample_gatk_vcf = '{}.GATK.vcf'.format(sample_base)
+sample_mapex_out = '{}.tsv'.format(sample_base)
 
 ############################
 # Generate the commands
@@ -110,139 +111,120 @@ sample_mapex_out = sample_base + '.tsv'
 # General purpose module load of pdx-exome seq conda env
 conda_build = ['m', 'load', 'python/3.5-Anaconda', '&&',
                'source', 'activate', conda_env, '&&']
-
 mapex_build = ['m', 'load', 'blast+/2.6.0', '&&']
 
 # FastQC
-fastqc_com = [fastqc, sample_1, '-o', output_dir]
+if command == 'fastqc':
+    fastqc_com = [fastqc, sample_1, '-o', output_dir]
+    conda_build.extend(fastqc_com)
 
 # TrimGalore
-trimgalore_com = [trimgalore, '--paired', sample_1, sample_2,
-                  '--output_dir', output_dir,
-                  '--fastqc_args', '"--outdir results/fastqc_trimmed/"']
+if command == 'trimgalore':
+    trimgalore_com = [trimgalore, '--paired', sample_1, sample_2,
+                      '--output_dir', output_dir,
+                      '--fastqc_args', '"--outdir results/fastqc_trimmed/"']
+    conda_build.extend(trimgalore_com)
 
 # BWA mem
-if command == 'bwa':
+if command == 'mem':
     bwa_mem_com = [bwa, 'mem', '-t', '8', hg_ref,
                    os.path.join('processed', 'trimmed', sample_1),
                    os.path.join('processed', 'trimmed', sample_2), '>',
                    sample_sam]
+    conda_build.extend(bwa_mem_com)
 
 # samtools sort to bam
-# `-n` sorts by name, which is required for fixmate
-samtools_sort_bam_com = [samtools, 'view', '-bS',
-                         os.path.join('processed', 'sam', sample_1),
-                         '|', samtools, 'sort', '-n', '-', sample_sorted_bam]
+if command == 'sort_name':
+    # `-n` sorts by name, which is required for fixmate
+    samtools_sort_bam_com = [samtools, 'view', '-bS',
+                             os.path.join('processed', 'sam', sample_1), '|',
+                             samtools, 'sort', '-n', '-', sample_sorted_bam]
+    conda_build.extend(samtools_sort_bam_com)
 
 # samtools create fixmate bam
-samtools_fixmate_com = [samtools, 'fixmate',
-                        os.path.join('processed', 'bam', sample_1),
-                        sample_sorted_fixmate_bam]
+if command == 'fixmate':
+    samtools_fixmate_com = [samtools, 'fixmate',
+                            os.path.join('processed', 'bam', sample_1),
+                            sample_sorted_fixmate_bam]
+    conda_build.extend(samtools_fixmate_com)
 
 # samtools sort fixmated bam by position
-samtools_positionsort_com = [samtools, 'sort',
-                             os.path.join('processed', 'bam_fixmate',
-                                          sample_1),
-                             sample_sorted_positionsort_bam]
+if command == 'sort_position':
+    samtools_positionsort_com = [samtools, 'sort',
+                                 os.path.join('processed', 'bam_fixmate',
+                                              sample_1),
+                                 sample_sorted_positionsort_bam]
+    conda_build.extend(samtools_positionsort_com)
 
 # samtools remove duplicated reads
-samtools_rmdup_com = [samtools, 'rmdup',
-                      os.path.join('processed', 'bam_sort_position', sample_1),
-                      sample_markdup_bam]
-
-# indel realignment - create indel targets
-gatk_realigner_com = [gatk, '-T', 'RealignerTargetCreator', '-R', hg_ref,
-                      '-I', os.path.join('processed', 'bam_rmdup', sample_1),
-                      '-o', sample_indel_intervals]
+if command == 'rmdup':
+    samtools_rmdup_com = [samtools, 'rmdup',
+                          os.path.join('processed', 'bam_sort_position',
+                                       sample_1),
+                          sample_markdup_bam]
+    conda_build.extend(samtools_rmdup_com)
 
 # samtools create bai indexing in preparation for variant calling
 if command == 'index_bam':
     sample_1_file = os.path.join('processed', 'bam_rmdup', sample_1)
-    sample_bai_out_file = sample_markdup_bai
-elif command == 'index_bam_gatk':
-    sample_1_file = os.path.join('processed', 'gatk_bam', sample_1)
-    sample_bai_out_file = sample_bamindex_gatk
-else:
-    sample_1_file = sample_1
-    sample_bai_out_file = 'none'
+    samtools_baiindex_bam_com = [samtools, 'index', sample_1_file,
+                                 sample_markdup_bai]
+    conda_build.extend(samtools_baiindex_bam_com)
 
-samtools_baiindex_com = [samtools, 'index', sample_1_file, sample_bai_out_file]
+if command == 'index_bam_gatk':
+    sample_1_file = os.path.join('processed', 'gatk_bam', sample_1)
+    samtools_baiindex_bam_gatk_com = [samtools, 'index', sample_1_file,
+                                      sample_bamindex_gatk]
+    conda_build.extend(samtools_baiindex_bam_gatk_com)
 
 # picard add read groups - required for variant calling
-picard_addreadgroup_com = [picard, 'AddOrReplaceReadGroups',
-                           'I={}'.format(os.path.join('processed', 'bam_rmdup',
-                                                      sample_1)),
-                           'O={}'.format(sample_addreadgroup),
-                           'RGID={}'.format(sample_1),
-                           'RGLB=bwa-mem',
-                           'RGPL=illumina',
-                           'RGSM={}'.format(sample_1),
-                           'RGPU={}'.format(sample_1),
-                           'CREATE_INDEX=true',
-                           'VALIDATION_STRINGENCY=SILENT']
+if command == 'add_read_groups':
+    sample_1_file = os.path.join('processed', 'bam_rmdup', sample_1)
+    picard_addreadgroup_com = [picard, 'AddOrReplaceReadGroups',
+                               'I={}'.format(sample_1_file),
+                               'O={}'.format(sample_addreadgroup),
+                               'RGID={}'.format(sample_1),
+                               'RGLB=bwa-mem',
+                               'RGPL=illumina',
+                               'RGSM={}'.format(sample_1),
+                               'RGPU={}'.format(sample_1),
+                               'CREATE_INDEX=true',
+                               'VALIDATION_STRINGENCY=SILENT']
+    conda_build.extend(picard_addreadgroup_com)
 
 # call variants using GATK MuTect2
-gatk_variant_call = [gatk, '-T', 'MuTect2',
-                     '--num_cpu_threads_per_data_thread', '8',
-                     '--standard_min_confidence_threshold_for_calling', '20',
-                     '--min_base_quality_score', '20',
-                     '-I:tumor', os.path.join('processed', 'gatk_bam',
-                                              sample_1),
-                     '-o', sample_gatk_vcf,
-                     '-R', hg_ref]
+if command == 'mutect2':
+    gatk_variant_com = [gatk, '-T', 'MuTect2',
+                        '--num_cpu_threads_per_data_thread', '8',
+                        '--standard_min_confidence_threshold_for_calling', '20',
+                        '--min_base_quality_score', '20',
+                        '-I:tumor', os.path.join('processed', 'gatk_bam',
+                                                 sample_1),
+                         '-o', sample_gatk_vcf,
+                         '-R', hg_ref]
+    conda_build.extend(gatk_variant_com)
 
 # Remove mouse reads using MAPEX
-mapex_remove_mouse_com = [rscript, '--vanilla', 'util/mapex_wrapper.R',
-                          '--path_to_bam', mapex_bam,
-                          '--path_to_bam_index', mapex_bam_bai,
-                          '--path_to_vcf', mapex_vcf,
-                          '--blast_output', mapex_blast_out,
-                          '--blast', mapex_blast,
-                          '--results_output', sample_mapex_out,
-                          '--blast_db', combined_ref,
-                          '--num_threads', 8,
-                          '--mapq', 1]
-
-# Schedule a job based on the input command
-if command == 'fastqc':
-    submit_commands = [fastqc_com]
-if command == 'trimgalore':
-    # Extending to ensure cutadapt is in path
-    conda_build.extend(trimgalore_com)
-    submit_commands = [conda_build]
-elif command == 'mem':
-    conda_build.extend(bwa_mem_com)
-    submit_commands = [conda_build]
-elif command == 'sort_name':
-    conda_build.extend(samtools_sort_bam_com)
-    submit_commands = [conda_build]
-elif command == 'fixmate':
-    conda_build.extend(samtools_fixmate_com)
-    submit_commands = [conda_build]
-elif command == 'sort_position':
-    conda_build.extend(samtools_positionsort_com)
-    submit_commands = [conda_build]
-elif command == 'rmdup':
-    conda_build.extend(samtools_rmdup_com)
-    submit_commands = [conda_build]
-elif command == 'index_bam' or command == 'index_bam_gatk':
-    conda_build.extend(samtools_baiindex_com)
-    submit_commands = [conda_build]
-elif command == 'target_intervals':
-    conda_build.extend(gatk_realigner_com)
-    submit_commands = [conda_build]
-elif command == 'add_read_groups':
-    conda_build.extend(picard_addreadgroup_com)
-    submit_commands = [conda_build]
-elif command == 'mutect2':
-    conda_build.extend(gatk_variant_call)
-    submit_commands = [conda_build]
-elif command == 'mapex':
+if command == 'mapex':
+    mapex_remove_mouse_com = [rscript, '--vanilla', 'util/mapex_wrapper.R',
+                              '--path_to_bam', mapex_bam,
+                              '--path_to_bam_index', mapex_bam_bai,
+                              '--path_to_vcf', mapex_vcf,
+                              '--blast_output', mapex_blast_out,
+                              '--blast', mapex_blast,
+                              '--results_output', sample_mapex_out,
+                              '--blast_db', combined_ref,
+                              '--num_threads', 8,
+                              '--mapq', 1]
     mapex_build.extend(conda_build)
     mapex_build.extend(mapex_remove_mouse_com)
-    submit_commands = [mapex_build]
 
 if __name__ == '__main__':
+    if command = 'mapex':
+        submit_commands = [mapex_build]
+    else:
+        submit_commands = [conda_build]
+    # Submit jobs to cluster
     for com in submit_commands:
         schedule_job(command=com, name=schedule_name, python=python,
                      nodes=nodes, cores=cores, walltime=walltime)
