b8f0c39935951b0bbc1ad9543656dcf58b5bee82
Greg <gregory.way@gmail.com>
2017-11-15 18:50:44 -0500

    update variable names and clean

--- scripts/6.disambiguate_species.py
+++ scripts/6.disambiguate_species.py
@@ -20,7 +20,7 @@ parser.add_argument('-o', '--output_dir',
 parser.add_argument('-y', '--config_yaml',
                     help='Configuration variables for input',
                     default='discovery_variables.yml')
-parser.add_argument('-w', '--walltime',
+parser.add_argument('-w', '--walltime', default='01:00:00',
                     help='the amount of time alloted to the script')
 parser.add_argument('-n', '--nodes', default=1,
                     help='the number of nodes to allocate')
@@ -37,30 +37,21 @@ walltime = args.walltime
 nodes = str(args.nodes)
 cores = str(args.cores)
 
-base_dir = '/lorax/sanchezlab/shared/pdx_exomeseq'
-
-# Set important suffixes
-bam_suffix = '.fq.gz.sam_sorted.bam_sorted_fixmate.bam_positionsort.bam' \
-             '.bam_rmdup.bam.rg.bam'
-bai_suffix = '{}.bai'.format(bam_suffix)
-vcf_suffix = '{}.GATK.vcf'.format(bam_suffix)
-
-# sample_files will have entry format [base, bam, bai, vcf]
-
+# sample_files will have entry format [sample_id, hg_sam, mm_sam, out_prefix]
 sample_files = []
 for path, subdirs, files in os.walk(human_dir):
     for name in files:
-        base = name.split('.')[0]  # This extracts the sample ID
+        sample_id = name.split('.')[0]  # This extracts the sample ID
         human_sam = os.path.join(base_dir, human_dir, name)
         mouse_sam = os.path.join(base_dir, mouse_dir, name)
         output_prefix = os.path.join(base_dir, out_dir, name)
-        sample_files.append([base, human_sam, mouse_sam, output_prefix])
+        sample_files.append([sample_id, human_sam, mouse_sam, output_prefix])
 
 sample_files = sample_files[0:2]
 command_util = os.path.join('util', 'command_wrapper.py')
-for sample_base, human_sam, mouse_sam, output_prefix in sample_files:
+for sample_id, human_sam, mouse_sam, output_prefix in sample_files:
     com = ['python', command_util,
-           '--sample', sample_base,
+           '--sample', sample_id,
            '--command', 'disambiguate',
            '--disambiguate_human', human_sam,
            '--disambiguate_mouse', mouse_sam,
@@ -69,4 +60,5 @@ for sample_base, human_sam, mouse_sam, output_prefix in sample_files:
            '--walltime', walltime,
            '--nodes', nodes,
            '--cores', cores]
+    print(com)
     subprocess.call(com)
