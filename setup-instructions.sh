724c9feb166a21038706f239ee6883db9d21a2f0
Greg <gregory.way@gmail.com>
2017-11-15 10:15:27 -0500

    update setup instructions for environment

--- setup-instructions.sh
+++ setup-instructions.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-# setting up the conda environment is tricky on the cluster
+# Gregory Way 2017
+# pdx_exomeseq
+# setup-instructions.sh
+#
+# Setting up environment
 
-# Step 1: load the python anaconda module
+# Step 1: Load the python anaconda module
 m load python/3.5-Anaconda
 
-# Step 2: activate the conda environment
+# Step 2: Activate the conda environment
 conda env create --force --file environment.yml
 source activate pdx-exomeseq
 
-# Step 3: Unload the anaconda module
-m unload python/3.5-Anaconda
+# Step 3: Register GATK
+# MANUAL STEP: Download Gatk3.8-0 and move to `modules`
+# Download from: https://software.broadinstitute.org/gatk/download/
+gatk-register modules/GenomeAnalysisTK-3.8-0.tar.bz2
 
-# Step 4: Setup channels
-conda config --add channels conda-forge
-conda config --add channels defaults
-conda config --add channels r
-conda config --add channels bioconda
-
-# Step 5: Install bioconda packages
-conda install ngs-disambiguate
-
+# Step 4: Install custom MAPEXR build from source
+# wget --directory-prefix modules/ https://bitbucket.org/bmannakee/mapexr/get/da36687d4585.zip
+Rscript util/install_mapexr.R
